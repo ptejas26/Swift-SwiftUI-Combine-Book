5e4c4ee45131ba1b576ea7cbdc380e94ae3238d6
ptejas26 <tejas.patelia@gmail.com>
2023-12-25 17:48:32 -0500

    Initial Commit

---
+++ Combine-Book-Course/Combine_Book_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_Book_CourseApp.swift
+//  Combine-Book-Course
+//
+//  Created by Tejas on 2023-12-25.
+//
+
+import SwiftUI
+
+@main
+struct Combine_Book_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
